310c3a9d0525740e4c3379a5dd44e7c391b3d6bd
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2019-01-02 14:30:30 -0600

    Add Session actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
-  def new
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      # Log in
+    else
+      flash[:danger] = "Invalid user or password."
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
   end
 end
